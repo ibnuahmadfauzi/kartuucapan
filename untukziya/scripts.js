3c96f66706cb5d9764a819347058311f8ac1cb3c
ibnuahmadfauzi <ibnu7897@gmail.com>
2024-10-28 23:02:28 +0700

    modification

--- untukziya/scripts.js
+++ untukziya/scripts.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("bgMusic").play();
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.getElementById("bgMusic").play();
+// });
